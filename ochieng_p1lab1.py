3bc2820bda6421ba568c9380af16c8e8e3c87525
Abraham-2099 <ochienga9179@student.faytechcc.edu>
2026-09-13 22:52:33 -0400

    first python assignment
    taking input from user and printing output

---
+++ ochieng_p1lab1.py
@@ -0,0 +1,9 @@
+# Abraham Ochieng
+#9/10/26
+#P1LAB1
+# input and print 
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hello,",first_name,last_name +"!","welcome to CTI-110!")
